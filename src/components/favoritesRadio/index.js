a1364cea442479c7aa689ab5b937d4be7ca95c31
Adenilson Silva <adenilson.silva.dev3561@gmail.com>
2026-05-01 15:45:26 -0300

    Implementation screen favorites

---
+++ src/components/favoritesRadio/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import { Feather } from "@expo/vector-icons";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+function FavoritesRadio({ data }) {
+  const [heart, setHeart] = useState(null);
+
+  const navigation = useNavigation();
+  function favoritar() {
+    setHeart(!heart);
+  }
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.containerNameRadio}
+        onPress={() => navigation.navigate("Player")}
+      >
+        <View style={styles.areaLogo}>
+          <Image style={styles.logoRadio} source={{ uri: data.image }} />
+        </View>
+        <Text>{data.name}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={favoritar}>
+        <Feather
+          name="heart"
+          size={30}
+          color={heart === true ? "red" : "#dcdcdc"}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    elevation: 2,
+    backgroundColor: "#ffffff",
+    padding: 4,
+    borderRadius: 10,
+    marginTop: 8,
+  },
+
+  containerNameRadio: {
+    width: "90%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  areaLogo: {
+    width: 80,
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginRight: 16,
+  },
+
+  logoRadio: {
+    width: 50,
+    height: 50,
+    objectFit: "contain",
+    marginRight: 16,
+  },
+});
+export default FavoritesRadio;
